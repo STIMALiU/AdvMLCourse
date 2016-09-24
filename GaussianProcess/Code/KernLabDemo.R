5db2513ee9eab1c3c21b479793b080da324fef8d
Mattias Villani <mattias.villani@gmail.com>
2016-09-24 02:41:10 +0200

    Major revision to this file

--- GaussianProcess/Code/KernLabDemo.R
+++ GaussianProcess/Code/KernLabDemo.R
@@ -1,45 +1,142 @@
-# Kernlab demo for Gaussian processes regression and classfication
+##############################################################################################
+## Kernlab demo for Gaussian processes regression and classfication
+## Author: Mattias Villani, Linkoping University. http://mattiasvillani.com
+##############################################################################################
 
+
+#############################################################################
+###    Prelims: Setting path and installing/loading packages            #####
+#############################################################################
 setwd('/Users/mv/Dropbox/Teaching/AdvML/GaussianProcess/Code')
 #install.packages('kernlab')
 #install.packages("AtmRay") # To make 2D grid like in Matlab's meshgrid
-
 library(kernlab)
 library(AtmRay)
 
-### Regression on the LIDAR data  ###
-lidarData <- read.table('LidarData', header = T)
+
+###############################################
+###    Messin' around with kernels        #####
+###############################################
+# This is just to test how one evaluates a kernel function
+# and how one computes the covariance matrix from a kernel function
+X <- matrix(rnorm(12), 4, 3)
+Xstar <- matrix(rnorm(15), 5, 3)
+ell <- 1
+SEkernel <- rbfdot(sigma = 1/(2*ell^2)) # Note how I reparametrize the rbfdor (which is the SE kernel) in kernlab
+SEkernel(1,2) # Just a test - evaluating the kernel in the points x=1 and x'=2
+# Computing the whole covariance matrix K from the kernel. Just a test.
+K <- kernelMatrix(kernel = SEkernel, x = X, y = Xstar) # So this is K(X,Xstar)
+
+# Own implementation of Matern with nu = 3/2 (See RW book equation 4.17)
+# Note that a call of the form kernelFunc <- Matern32(sigmaf = 1, ell = 0.1) returns a kernel FUNCTION.
+# You can now evaluate the kernel at inputs: kernelFunc(x = 3, y = 4).
+# Note also that class(kernelFunc) is of class "kernel", which is a class defined by kernlab.
+Matern32 <- function(sigmaf = 1, ell = 1) 
+{
+  rval <- function(x, y = NULL) {
+      r = sqrt(crossprod(x-y));
+      return(sigmaf^2*(1+sqrt(3)*r/ell)*exp(-sqrt(3)*r/ell))
+    }
+  class(rval) <- "kernel"
+  return(rval)
+} 
+
+
+# Testing our own defined kernel function.
+X <- matrix(rnorm(12), 4, 3) # Simulating some data
+Xstar <- matrix(rnorm(15), 5, 3)
+MaternFunc = Matern32(sigmaf = 1, ell = 2) # MaternFunc is a kernel FUNCTION
+MaternFunc(c(1,1),c(2,2)) # Evaluating the kernel in x=c(1,1), x'=c(2,2)
+# Computing the whole covariance matrix K from the kernel.
+K <- kernelMatrix(kernel = MaternFunc, x = X, y = Xstar) # So this is K(X,Xstar)
+
+
+
+
+
+###############################################
+###      Regression on the LIDAR data       ###
+###############################################
+lidarData <- read.table('https://raw.githubusercontent.com/STIMALiU/AdvMLCourse/master/GaussianProcess/Code/LidarData', 
+                        header = T)
 LogRatio <- lidarData$LogRatio
 Distance <- lidarData$Distance
 
+# Estimating the noise variance from a third degree polynomial fit
+polyFit <- lm(LogRatio ~  Distance + I(Distance^2) + I(Distance^3) )
+sigmaNoise = sd(polyFit$residuals)
+
 plot(Distance,LogRatio)
-GPfit <- gausspr(Distance, LogRatio)
-ytest <- predict(GPfit, Distance)
-lines(Distance,ytest,col="red")
+
+# Fit the GP with built in Square expontial kernel (called rbfdot in kernlab)
+ell <- 2
+GPfit <- gausspr(Distance, LogRatio, kernel = rbfdot, kpar = list(sigma = 1/(2*ell^2)), var = sigmaNoise^2)
+meanPred <- predict(GPfit, Distance) # Predicting the training data. To plot the fit.
+lines(Distance, meanPred, col="blue", lwd = 2)
+
+# Fit the GP with home made Matern
+sigmaf <- 1
+ell <- 2
+# GPfit <- gausspr(Distance, LogRatio, kernel = Matern32(ell=1)) # NOTE: this also works and is the same as the next line.
+GPfit <- gausspr(Distance, LogRatio, kernel = Matern32, kpar = list(sigmaf = sigmaf, ell=ell), var = sigmaNoise^2) 
+meanPred <- predict(GPfit, Distance)
+lines(Distance, meanPred, col="purple", lwd = 2)
+
+# Trying another length scale
+sigmaf <- 1
+ell <- 1
+GPfit <- gausspr(Distance, LogRatio, kernel = Matern32, kpar = list(sigmaf = sigmaf, ell=ell), var = sigmaNoise^2) 
+meanPred <- predict(GPfit, Distance)
+lines(Distance, meanPred, col="green", lwd = 2)
+
+# And now with a different sigmaf
+sigmaf <- 0.1
+ell <- 2
+GPfit <- gausspr(Distance, LogRatio, kernel = Matern32, kpar = list(sigmaf = sigmaf, ell=ell), var = sigmaNoise^2) 
+meanPred <- predict(GPfit, Distance)
+lines(Distance, meanPred, col="black", lwd = 2)
 
 
-#### Regression on the Canadian wages data ####
-wagesData <- read.table('CanadianWages.dat', header = T)
-logWage <- wagesData$logWage
-age <- wagesData$age
-plot(age, logWage)
-GPfit <- gausspr(logWage ~ age, kernel = 'rbfdot', par = list(sigma = 1))
-meanPred <- predict(GPfit, age)
-lines(age, meanPred, col="red")
+
+## THE MARGINAL LIKELIHOOD STUFF BELOW GIVES ME STRANGE RESULTS. IGNORE FOR NOW ##
+
+## Defining the log marginal likelihood function
+MargLikeGPReg <- function(X, y, kernel, kpar, sigmaNoise){
+  kernelFunc <- do.call(kernel, kpar)
+  K <- kernelMatrix(kernel = kernelFunc, x = X) # K(X,X)
+  n <- length(y)
+  
+  L = chol(K+sigmaNoise^2*diag(n));
+  alpha_ <- solve(t(L),solve(L,y))
+  return( -0.5*crossprod(y,alpha_) -sum(log(diag(L))) )
+}
+
+# Evalating the log marginal likelihood over a grid of values
+sigmafGrid <- seq(0.1,1,by = 0.2)
+ellGrid <- seq(1,10,by = 1)
+lml <- matrix(NA, length(ellGrid), length(sigmafGrid))
+for (j in 1:length(sigmafGrid)){
+  for (i in 1:length(ellGrid)){
+    lml[i,j] <- MargLikeGPReg(X = Distance, y = LogRatio, kernel = Matern32, kpar = list(sigmaf = sigmafGrid[j], ell = ellGrid[i]), sigmaNoise)
+  }
+}
+# Find the hyperparametes with largest log marginal likelihood over the grid
+optEll <- ellGrid[which(lml == max(lml), arr.ind =TRUE)[1]]
+optSigmaf <- sigmafGrid[which(lml == max(lml), arr.ind =TRUE)[2]]
+message(c("The optimal hyperparameters are: ell = ",optEll," and ", "sigmaf = ",optSigmaf))
+
+# Plotting the fit based on the optimal hyperparameters
+GPfit <- gausspr(Distance, LogRatio, kernel = Matern32, kpar = list(sigmaf = optSigmaf, ell=optEll), var = sigmaNoise^2) 
+meanPred <- predict(GPfit, Distance)
+lines(Distance, meanPred, col="red", lwd = 3)
 
 
-### Regression on the Japan temperature data ####
-tempData <- read.table('JapanTemp.dat', header = TRUE)
-temp <- tempData$temp
-time <- tempData$time
-plot(time, temp)
-GPfit <- gausspr(temp ~ time, kernel = 'rbfdot', par = list(sigma = 1))
-meanPred <- predict(GPfit, time)
-lines(time, meanPred, col="red")
 
 
 
-#### Classification on Iris data ###
+###############################################
+####       Classification on Iris data      ###
+###############################################
 data(iris)
 GPfitIris <- gausspr(Species ~  Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, data=iris)
 GPfitIris
